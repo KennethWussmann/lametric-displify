186f39de08b47429d26eb9a7e5ab3b86f91008be
Kenneth Wußmann <kenneth@wussmann.net>
2018-11-03 09:50:43 +0100

    Fixed test

--- src/test/java/de/ketrwu/lametric/api/DisplifyHandlerTest.java
+++ src/test/java/de/ketrwu/lametric/api/DisplifyHandlerTest.java
@@ -16,6 +16,7 @@ class DisplifyHandlerTest {
     void handleRequest() throws Exception {
         DisplifyHandler handler = new DisplifyHandler();
         LaMetricRequest request = new LaMetricRequest(
+                "c7f89e18-1c37-4264-8696-4bd3ff1e8aa6",
                 "device-0001",
                 "ce3b80ec-73ab-4d40-9578-d6afa391ff1b",
                 "Bearer e49e9b0b-3ce7-4b16-8536-6e165d94c191"
